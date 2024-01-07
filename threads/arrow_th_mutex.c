9a652090dae2bf44a599d1ea938c8b9765bde76f
Hafsa <101563334+hafsa-dreamyland@users.noreply.github.com>
2024-01-07 18:54:39 +0000

    thread-mutex-sem

---
+++ threads/arrow_th_mutex.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+
+pthread_mutex_t m;
+
+void* print_rightarrow (void* unused) {
+  while (1) {
+    pthread_mutex_lock(&m);
+    fputc ('-', stdout);
+    fputc ('>', stdout);
+    fputc ('\n', stdout);
+    pthread_mutex_unlock(&m);
+  }
+    return NULL;
+}
+
+
+int main () {
+    pthread_t thread_id;
+    pthread_mutex_init(&m,NULL);
+    
+    pthread_create(&thread_id, NULL, &print_rightarrow, NULL);
+
+    while (1) {
+      pthread_mutex_lock(&m);
+      fputc ('<', stdout);
+      fputc ('=', stdout);
+      fputc ('\n', stdout);
+      pthread_mutex_unlock(&m);
+    }
+    return 0;
+}
